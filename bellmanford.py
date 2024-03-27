f3e0e4a1005c31e3d5a5e04772fefa9985843d6f
Roman Kashitsyn <roman.kashitsyn@gmail.com>
2024-03-27 10:29:30 +0100

    bellmandford: use adjucency list as input

--- bellmanford.py
+++ bellmanford.py
@@ -1,10 +1,11 @@
-def bellmanford(nv, es, s):
-    d = [float('inf') for _ in range(nv)]
+def bellmanford(g, s):
+    d = [float('inf') for _ in g]
     d[s] = 0
-    p = [None for _ in range(nv)]
-    for _ in range(nv - 1):
-        for u, v, w in es:
-            if d[u] + w < d[v]:
-                d[v] = d[u] + w
-                p[v] = u
+    p = [None for _ in g]
+    for _ in range(len(g) - 1):
+        for u, adj in enumerate(g):
+            for v, w in adj:
+                if d[u] + w < d[v]:
+                    d[v] = d[u] + w
+                    p[v] = u
     return (p, d)
